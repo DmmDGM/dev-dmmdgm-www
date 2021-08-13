0d629eac54d2666acede61a6eb2dd9acf767cee8
DmmD <dm12332131mdgaming@gmail.com>
2021-08-12 22:19:38 -0400

    Saving

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // Imports
 const config = require("./config.json");
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 // Variables
 let app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "/public/")));
+app.use("/", require("./public/routes/base.js"));
 
 // Listens
 app.listen(config.port, () => {
+    console.clear();
     console.log(`App is now online!`);
 });
---
+++ public/routes/base.js
@@ -0,0 +1,14 @@
+// Imports
+const { Router } = require("express");
+const path = require("path");
+
+// Variables
+let router = Router();
+
+// Routes
+router.get("/", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "../html/home.html"));
+});
+
+// Exports
+module.exports = router;
