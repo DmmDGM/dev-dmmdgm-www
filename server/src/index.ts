ab1e652718ccd028a9b50cc954d1fbe1e45578f3
DmmDGM <dm12332131mdgaming@gmail.com>
2025-08-16 23:09:32 -0400

    formatted server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,16 @@
+// Imports
+import staticPlugin from "@elysiajs/static";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// Defines app
+const app = new Elysia()
+    .use(staticPlugin({
+        assets: "../client/dist/",
+        prefix: "/"
+    }))
+    .listen(3000);
 
+// Prints console
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
