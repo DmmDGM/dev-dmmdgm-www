e40fe1e571827b3150271cd53bc65afbd2e51521
dmmdgm <dmmdgm@dmmdgm.dev>
2026-08-10 23:29:41 -0400

    framework done

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,27 @@ import "./assets/css/index.css";
 // Defines website
 function Website() {
 	return <>
-		<nav>hello</nav>
+		{/* Header */}
+		<header>
+			<a>World</a>
+			<a>About</a>
+			<img src="/happi.png"/>
+			<a>Projects</a>
+			<a>idk what to put here</a>
+		</header>
+
+		<main>
+			{/* Game */}
+			<canvas></canvas>
+		</main>
+		
+		{/* Footer */}
+		<footer>
+			<span>
+				Hand Written :3 <a href="https://github.com/DmmDGM/dev-dmmdgm-www" target="_blank" rel="noopener noreferrer">Source Code</a> (cant believe this is the exception now)
+			</span>
+			<span>DmmD GM - Coming Soon to Your Nearest Supermarket @ 2026</span>
+		</footer>
 	</>;
 }
 
