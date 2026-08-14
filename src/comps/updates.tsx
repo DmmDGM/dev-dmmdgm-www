24a9b610120332d980fa56ecc32d1cae9cf99bd5
dmmdgm <dmmdgm@dmmdgm.dev>
2026-08-13 20:39:40 -0400

    updated update banner

--- src/comps/updates.tsx
+++ src/comps/updates.tsx
@@ -6,6 +6,11 @@ export default function() {
     // Creates html
     return <div id="updates">
         <h3>Updates</h3>
+        <span>2026-08-13</span>
+        <span>i added wasd walking</span>
+        <span>its gonna be a cool walking simulator about a :3 character</span>
+        <span>inspired by yume 2kki, undertale (not deltarune im not touching deltarune until the community's hype is over because the meme kids are just so annoying and overstimulated and brainrotted and i rather wait until everything settles down so the only people remaining are the people who actually care and enjoy the game and want to dig deeper about its music and its lore instead of the retarded gen alpha apes that spam WOOOOOOO FUNNI NEW META CHARACTER HAHAHAHAHA [INSERT TWENTY THOUSAND VINE BOOM BRAINROT CONTENT FLASHY LIGHT OVERLY-SATURATED AI SLOP MEMES] every time theres something goofy), oneshot (niko cute, niko is justice), and blue archive</span>
+        <br/>
         <span>2026-08-12</span>
         <span>i added 'about' 'projects' 'roadmaps' and 'updates' tabs</span>
         <span>you're reading them right now actually :3</span>
